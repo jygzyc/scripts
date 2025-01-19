87681e5172ce1ff0306a12f5d68193876aa37767
ecool <jyg.zyc@outlook.com>
2025-01-20 00:29:37 +0800

    20250120 update

--- wsl/README.md
+++ wsl/README.md
@@ -1,3 +1,7 @@
 # WSL config
 
 [configuration-settings-for-wslconfig](https://learn.microsoft.com/zh-cn/windows/wsl/wsl-config#configuration-settings-for-wslconfig)
+
+## Proxy
+
+First run `forward.bat` in Windows host system, then run `proxy.sh` in **proxy** directory. Remember check your ports!
--- wsl/forward.bat
+++ wsl/forward.bat
@@ -4,7 +4,11 @@
 @REM IPV4 Port Forwarding for Windows to WSL
 @REM netsh interface portproxy add v4tov4 listenport=[Windows port] listenaddress=0.0.0.0 connectport=[WSL port] connectaddress=[wsl ip]
 for /f "tokens=*" %a in ('wsl hostname -I') do @set WSL_IP=%a
+@REM Android ADB Port Forwarding
 netsh interface portproxy add v4tov4 listenport=5037 listenaddress=0.0.0.0 connectport=5037 connectaddress=%WSL_IP%
+@REM IDA Pro Port Forwarding
 netsh interface portproxy add v4tov4 listenport=20742 listenaddress=0.0.0.0 connectport=20742 connectaddress=%WSL_IP%
 netsh interface portproxy add v4tov4 listenport=20743 listenaddress=0.0.0.0 connectport=20743 connectaddress=%WSL_IP%
+@REM Clash Port Forwarding
+netsh interface portproxy add v4tov4 listenport=7890 listenaddress=0.0.0.0 connectport=7890 connectaddress=%WSL_IP%
 pause
